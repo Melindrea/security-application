afc9e1bcfb59522043194f1f08bd1c8beab25858
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-09-08 20:32:00 +0200

    Changing workbench to use name-email

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
     |
     */
 
-    'name' => '<%= name %>',
+    'name' => 'Marie Hogebrandt',
 
     /*
     |--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
     |
     */
 
-    'email' => '<%= email %>',
+    'email' => 'iam@mariehogebrandt.se',
 
 );
--- app/filters.php
+++ app/filters.php
@@ -87,7 +87,7 @@ Route::filter(
     'csrf',
     function () {
         if (Session::token() != Input::get('_token')) {
-            throw new \Illuminate\Session\TokenMismatchException;
+            throw new Illuminate\Session\TokenMismatchException;
         }
     }
 );

2a1004e3bf24873943d7a7e735f55bf2d3d4fe90
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-09-16 16:32:25 +0200

    moving from public to public_html

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,7 +22,7 @@ module.exports = function (grunt) {
     // configurable paths
     var yeomanConfig = {
         app: 'app/src',
-        dist: 'public/assets',
+        dist: 'public_html/assets',
         php: 'app'
 
     };

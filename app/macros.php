d37e334352b00c1454eea3d67e4dea5333e1de15
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-10-24 16:51:21 +0200

    Adding analytics as a partial

--- app/macros.php
+++ app/macros.php
@@ -226,16 +226,9 @@ HTML::macro(
 */
 HTML::macro(
     'ga',
-    function ($ua) { ?>
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create', '<?php echo $ua ?>');ga('send','pageview');
-        </script>
-   <?php
+    function ($ua) {
+        return View::make('partials.analytics.ga')
+        ->with('ua', $ua);
     }
 );
 

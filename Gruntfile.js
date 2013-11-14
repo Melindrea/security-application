67f6d66fb59aea1fddd68fddf8297789880a84b7
Marie Hogebrandt <iam@mariehogebrandt.se>
2013-11-14 20:19:45 +0100

    Adjusting stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -174,7 +174,8 @@ module.exports = function (grunt) {
         },
         jshint: {
             options: {
-                jshintrc: '.jshintrc'
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
             },
             all: [
                 'Gruntfile.js',
